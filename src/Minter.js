90aacd8cd5835576c506ca3cee3bed4b5fb60f6b
Kimonic <aimuaimarengiaye@gmail.com>
2022-11-05 17:37:06 -0500

    first commit number 24

---
+++ src/Minter.js
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected, mintNFT } from "./utils/interact.js";
+
+const Minter = (props) => {
+
+  //State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+  
+
+  useEffect(() => {
+    async function fetchWallet() {
+      // You can await here
+      const {address, status} = await getCurrentWalletConnected();
+      setWallet(address)
+      setStatus(status);
+    }
+    fetchWallet();
+    addWalletListener();
+  }, []); 
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank"  rel="noreferrer" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+  
+ 
+  
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onMintPressed = async () => {
+    const { success, status } = await mintNFT(url, name, description);
+    setStatus(status);
+    if (success) {
+      setName("");
+      setDescription("");
+      setURL("");
+    }
+  };
+
+  
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">🧙‍♂️ Polygon Bootcamp Minter </h1>
+      <p id="desc">
+        Simply add your asset's link, name, and description, then press "Mint."
+      </p>
+      <form>
+        <h2>🖼 Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>🤔 Name: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Pick a cool name!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2>✍️ Description: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Definitely not cooler than cryptokitties ;)"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
